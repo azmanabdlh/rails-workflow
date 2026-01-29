7b549d658cbd2ed1f3bf2a7ce735ddb79b751325
Azman <macbookpro@MacBook-Pro-13.local>
2026-01-29 15:12:57 +0900

    typo

--- app/controllers/candidate_controller.rb
+++ app/controllers/candidate_controller.rb
@@ -3,7 +3,7 @@ class CandidateController < ApplicationController
   def new
     req = params.permit(:candidate_id, :stage_id)
 
-    from = new_candidate(req[:candidate_id]).current_phase
+    from = Candidate.find(req[:candidate_id]).current_phase
     to = Stage.find(req[:stage_id])
 
     unless from.can_transition_to?(to)
@@ -15,10 +15,6 @@ class CandidateController < ApplicationController
 
     # update reviewers
 
-  end
-
-  def new_candidate(id)
-    Candidate.find(req[:candidate_id])
   end
 
 end
