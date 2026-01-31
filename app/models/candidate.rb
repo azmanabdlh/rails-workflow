730ad993bbc0afddaac70e26c5383018119eba2a
Azman <macbookpro@MacBook-Pro-13.local>
2026-01-31 17:32:09 +0900

    fix: last workflow stage

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,6 +2,6 @@ class Candidate < ApplicationRecord
   has_many :workflows
 
   def last_workflow_stage
-    workflows.find_by(stage_id: current_stage_id, exited_at: nil)
+    workflows.find_by(stage_id: current_stage_id)
   end
 end
